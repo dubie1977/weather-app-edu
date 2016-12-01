26805732869277f73accc5f635cd2740079813b8
dubie <dubie@dubies-Mac.local>
2016-12-01 14:57:44 -0800

    set up basic weather module and linked it in the root

---
+++ app/weather-widget/component/weather.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    moduleId: module.id,
+    selector: 'weather-widget',
+    templateUrl: 'weather.component.html',
+    styleUrls: ['weather.component.css']
+})
+
+export class WeatherComponent{ }
