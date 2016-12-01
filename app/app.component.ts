77eb07184ca0b7e0dfafb3173e94f143c1bd3bc5
dubie <dubie@dubies-Mac.local>
2016-12-01 15:58:01 -0800

    template for widget layed out

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,11 +5,16 @@ import { Component } from '@angular/core';
     selector: 'my-app',
     template: `
         <div class="container">
-            <div class="col-xs-3">
+            <div class="col-xs-4">
                 <weather-widget></weather-widget>
             </div>
         </div>
-    `
+    `,
+    styles:[`
+        .container {
+            padding-top: 3rem;
+        }
+    `]
 })
 
 export class AppComponent{ }
